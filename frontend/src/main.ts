dd23d8fe47f188e639936496fd5babc6f86ac158
Quynh Ngo <ngophuongquynh25@gmail.com>
2023-08-20 16:23:53 +0200

    added tailwind css and axios

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/css/main.css'
+import './assets/css/tailwind.css'
 import App from './App.vue'
 
 import router from './router'; // Import the router configuration
