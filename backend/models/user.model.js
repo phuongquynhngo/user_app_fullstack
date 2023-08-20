52ce03ba06329974763d0c582ce67deb412749bf
Quynh Ngo <ngophuongquynh25@gmail.com>
2023-08-20 14:28:20 +0200

    define the mongoose model and create the controller

---
+++ backend/models/user.model.js
@@ -0,0 +1,18 @@
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+        {
+          name: String,
+          role: String,
+          online_status: Boolean
+        },
+        { timestamps: true }
+      );
+    schema.method("toJSON", function() {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+      });
+    const User = mongoose.model("user", schema);
+  
+    return User;
+  };
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
 
 // Configure allowed origins
-var corsOptions = {
+const corsOptions = {
   origin: "http://localhost:8081" 
   //allow requests from the specified origin (http://localhost:8081)
 };
@@ -13,19 +16,42 @@ var corsOptions = {
 // Apply CORS with the specified options
 app.use(cors(corsOptions));
 
+// set port, 
+dotenv.config({path:'config.env'});
+const PORT = process.env.PORT || 8080;
+
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to user fullstack application." });
 });
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
+
+
+//log request
+app.use(morgan('tiny'));
+
+//listen for requests
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
